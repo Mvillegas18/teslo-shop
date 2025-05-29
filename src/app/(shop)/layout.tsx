22a7d55c09f9a8a7f6447bdf46649da334751462
Miguel Angel Villegas <miguelvillegas1121878@correo.itm.edu.co>
2025-05-29 18:21:42 -0500

    Add layout and page components for shop and authentication sections

--- src/app/(shop)/layout.tsx
+++ src/app/(shop)/layout.tsx
@@ -3,10 +3,5 @@ export default function ShopLayout({
 }: {
 	children: React.ReactNode;
 }) {
-	return (
-		<div>
-			<h1>Hello layout</h1>
-			{children}
-		</div>
-	);
+	return <main className="bg-red-500 min-h-screen">{children}</main>;
 }
