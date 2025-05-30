f0114b317878876312e11aa7cd6951b85c5613e5
Miguel Angel Villegas <miguelvillegas1121878@correo.itm.edu.co>
2025-05-29 20:32:30 -0500

    Update main layout styles in Home component

--- src/app/(shop)/page.tsx
+++ src/app/(shop)/page.tsx
@@ -2,7 +2,7 @@ import { titleFont } from '@/config/fonts';
 
 export default function Home() {
 	return (
-		<main className="">
+		<main className="inset-0 flex items-center justify-center">
 			<h1>Hola mundo</h1>
 			<h1 className={` ${titleFont.className}`}>Hola mundo</h1>
 		</main>
