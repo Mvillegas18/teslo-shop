251565b68bbb8cdd3e9c5e7319f12cf9b1809595
Miguel Angel Villegas <miguelvillegas1121878@correo.itm.edu.co>
2025-06-01 19:34:23 -0500

    Refactor CategoryPage to use Category type and improve product filtering logic

--- src/app/(shop)/category/[id]/page.tsx
+++ src/app/(shop)/category/[id]/page.tsx
@@ -1,18 +1,37 @@
+import { ProductGrid, Title } from '@/components';
+import { Category } from '@/interfaces';
+import { initialData } from '@/seed/seed';
 import { notFound } from 'next/navigation';
 
 interface Props {
-	params: { id: string };
+	params: { id: Category };
 }
+const products = initialData.products;
 
 export default async function CategoryPage({ params }: Props) {
 	const { id } = await params;
-	if (id === 'kids') {
+	const productsCategories = products.filter(
+		(product) => product.gender === id
+	);
+
+	if (id !== 'kid' && id !== 'women' && id !== 'men') {
 		notFound();
 	}
+	const labels: Record<Category, string> = {
+		kid: 'Niños',
+		women: 'Mujeres',
+		men: 'Hombres',
+		unisex: 'Unisex',
+	};
 
 	return (
-		<div>
-			<h1>Category page: {id}</h1>
-		</div>
+		<>
+			<Title
+				title={`${labels[id]}`}
+				subtitle={`Todos los productos de la categoria de ${labels[id]}`}
+				className="mb-2"
+			/>
+			<ProductGrid products={productsCategories} />
+		</>
 	);
 }
