adccea21be7ff9a4362a0e90e4ae2a6234bb0776
Miguel Angel Villegas <miguelvillegas1121878@correo.itm.edu.co>
2025-06-01 21:14:07 -0500

    Adjust PageNotFound component height for improved layout

--- src/components/ui/not-found/PageNotFound.tsx
+++ src/components/ui/not-found/PageNotFound.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link';
 
 export const PageNotFound = () => {
 	return (
-		<div className="flex flex-col-reverse md:flex-row h-[700px] w-full justify-center items-center align-middle">
+		<div className="flex flex-col-reverse md:flex-row h-[600px] w-full justify-center items-center align-middle">
 			<div className="text-center px-5 mx-5">
 				<h2 className={`${titleFont.className} antialiased text-9xl`}>
 					404
