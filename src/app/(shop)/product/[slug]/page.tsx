d551a77f3b1bd3b1b392f0cf8822d0c611c7c243
Miguel Angel Villegas <miguelvillegas1121878@correo.itm.edu.co>
2025-06-01 21:14:03 -0500

    Refactor ProductPage to fetch product data and display details dynamically

--- src/app/(shop)/product/[slug]/page.tsx
+++ src/app/(shop)/product/[slug]/page.tsx
@@ -1,8 +1,42 @@
-export default function ProductPage() {
+import { titleFont } from '@/config/fonts';
+import { initialData } from '@/seed/seed';
+import { notFound } from 'next/navigation';
+
+interface Props {
+	params: { slug: string };
+}
+
+export default async function ProductPage({ params }: Props) {
+	const { slug } = await params;
+
+	const products = initialData.products;
+	const product = products.find((product) => product.slug === slug);
+
+	if (!product) {
+		notFound();
+	}
+
 	return (
-		<div>
-			<h1>Product Page</h1>
-			<p>This is the product page for a specific product.</p>
+		<div className="mt-5 mb-20 grid grid-cols-1 md:grid-cols-3 gap-3">
+			<div className="col-span-1 md:col-span-2">Hola</div>
+
+			<div className="col-span-1 px-5">
+				<h1
+					className={`${titleFont.className} antialiased font-bold text-xl`}
+				>
+					{product.title}
+				</h1>
+				<p className="text-lg mb-5">
+					{product.price.toLocaleString('en-US', {
+						style: 'currency',
+						currency: 'USD',
+					})}
+				</p>
+
+				<button className="btn-primary my-5">Agregar al carrito</button>
+				<h3 className="font-bold text-sm">Descripcion</h3>
+				<p className="font-light">{product.description}</p>
+			</div>
 		</div>
 	);
 }
