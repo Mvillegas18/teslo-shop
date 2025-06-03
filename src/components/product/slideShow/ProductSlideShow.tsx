f5c1b4c7bd9935c4269c562a8f722ca581fba03a
Miguel Angel Villegas <miguelvillegas1121878@correo.itm.edu.co>
2025-06-02 19:55:31 -0500

    Refactor ProductSlideShow component to enhance image display functionality and integrate with ProductPage

--- src/components/product/slideShow/ProductSlideShow.tsx
+++ src/components/product/slideShow/ProductSlideShow.tsx
@@ -1,20 +1,76 @@
-import { Swiper, SwiperSlide } from 'swiper/react';
+'use client';
+import { Swiper as SwiperObject } from 'swiper';
 import 'swiper/css';
-export const ProductSlideShow = () => {
+import 'swiper/css/free-mode';
+import 'swiper/css/navigation';
+import 'swiper/css/thumbs';
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+import Image from 'next/image';
+import { useState } from 'react';
+import { Autoplay, FreeMode, Navigation, Thumbs } from 'swiper/modules';
+import './slideShow.css';
+
+interface Props {
+	images: string[];
+	title: string;
+	className?: string;
+}
+export const ProductSlideShow = ({ images, title, className }: Props) => {
+	const [thumbsSwiper, setThumbsSwiper] = useState<SwiperObject>();
+
 	return (
-		<>
+		<div className={` ${className}`}>
 			<Swiper
-				spaceBetween={50}
-				slidesPerView={3}
-				onSlideChange={() => console.log('slide change')}
-				onSwiper={(swiper) => console.log(swiper)}
+				style={
+					{
+						'--swiper-navigation-color': '#fff',
+						'--swiper-pagination-color': '#fff',
+					} as React.CSSProperties
+				}
+				spaceBetween={10}
+				navigation={true}
+				autoplay={{
+					delay: 2500,
+					disableOnInteraction: false,
+				}}
+				thumbs={{ swiper: thumbsSwiper }}
+				modules={[FreeMode, Navigation, Thumbs, Autoplay]}
+				className="mySwiper2"
 			>
-				<SwiperSlide>Slide 1</SwiperSlide>
-				<SwiperSlide>Slide 2</SwiperSlide>
-				<SwiperSlide>Slide 3</SwiperSlide>
-				<SwiperSlide>Slide 4</SwiperSlide>
-				...
+				{images.map((image) => (
+					<SwiperSlide key={image}>
+						<Image
+							src={`/products/${image}`}
+							alt={title}
+							width={1024}
+							height={800}
+							className="w-full h-full object-fill rounded-lg"
+						/>
+					</SwiperSlide>
+				))}
 			</Swiper>
-		</>
+			<Swiper
+				onSwiper={setThumbsSwiper}
+				spaceBetween={10}
+				slidesPerView={4}
+				freeMode={true}
+				watchSlidesProgress={true}
+				modules={[FreeMode, Navigation, Thumbs]}
+				className="mySwiper"
+			>
+				{images.map((image) => (
+					<SwiperSlide key={image}>
+						<Image
+							src={`/products/${image}`}
+							alt={title}
+							width={300}
+							height={300}
+							className="w-full h-full object-fill rounded-lg"
+						/>
+					</SwiperSlide>
+				))}
+			</Swiper>
+		</div>
 	);
 };
